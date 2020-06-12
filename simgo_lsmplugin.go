c418d8e63676f209c78b74784198794e963b683b
Tony Asleson <tasleson@redhat.com>
2020-06-12 15:31:58 -0500

    Use re-factored VolumeCreate method
    Signed-off-by: Tony Asleson <tasleson@redhat.com>

--- simgo_lsmplugin.go
+++ simgo_lsmplugin.go
@@ -89,15 +89,7 @@ func jobFree(jobID string) error {
 
 func volCreate(pool *lsm.Pool, volumeName string, size uint64,
 	provisioning lsm.VolumeProvisionType) (*lsm.Volume, *string, error) {
-
-	var volume lsm.Volume
-	jobID, error := state.c.VolumeCreate(pool, volumeName, size, provisioning,
-		false, &volume)
-
-	if jobID != nil {
-		return nil, jobID, error
-	}
-	return &volume, nil, error
+	return state.c.VolumeCreate(pool, volumeName, size, provisioning, false)
 }
 
 func volDelete(volume *lsm.Volume) (*string, error) {
